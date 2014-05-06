a6c44b94440aa7daa5029b9edae30a13aae8f2a4
Ruslan Nigmatullin <euroelessar@yandex.ru>
2014-05-06 16:10:49 +0400

    Added ability to specify library name's suffix
    Added cmake option JREEN_SUFFIX. It may be usefull for installing
    Qt4 and Qt5-based Jreen to system at the same time.

--- src/libjreen.pc.cmake
+++ src/libjreen.pc.cmake
@@ -3,10 +3,10 @@ exec_prefix=${CMAKE_INSTALL_PREFIX}/bin
 libdir=${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX}
 includedir=${CMAKE_INSTALL_PREFIX}/include
 
-Name: libjreen
+Name: libjreen${JREEN_SUFFIX}
 Description: Qt Jabber/XMPP extensible library
 Requires: QtCore QtNetwork
 Version: ${CMAKE_JREEN_VERSION_MAJOR}.${CMAKE_JREEN_VERSION_MINOR}.${CMAKE_JREEN_VERSION_PATCH}
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX} -ljreen
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX} -ljreen${JREEN_SUFFIX}
 Cflags: -I${CMAKE_INSTALL_PREFIX}/include
 
